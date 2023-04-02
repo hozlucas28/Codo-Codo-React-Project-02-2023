542902367a7502653474ba20e21ec0977c2e4513
hozlucas28 <hozlucas28@gmail.com>
2023-04-01 22:51:45 -0300

    Create constants

---
+++ src/constants/LINKS.js
@@ -0,0 +1,4 @@
+export const LINKS = {
+	author: 'https://github.com/hozlucas28',
+	repository: 'https://github.com/hozlucas28/Codo-Codo-React-Project-02-2023',
+}
---
+++ src/constants/TEXTS.js
@@ -0,0 +1,30 @@
+export const TEXTS = {
+	header: {
+		title: 'Todos',
+	},
+
+	main: {
+		todo: {
+			addCheckbox: {
+				input: {
+					placeholders: ['Buy milk', 'Give a lesson', 'Go to the party', 'Do the homework', 'Create a new project'],
+				},
+
+				btn: {
+					text: 'Add',
+					altText: 'Adds a new checkbox to the todo list',
+				},
+			},
+		},
+
+		addTodoBtn: {
+			text: 'Add todo',
+			altText: 'Adds a new todo list',
+		},
+	},
+
+	footer: {
+		copyright: 'Copyright © 2023 lucas hoz',
+		sourceCode: '[ View source code ]',
+	},
+}
