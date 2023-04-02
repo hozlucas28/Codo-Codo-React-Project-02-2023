90d1f2f240c30d4c764cacc66c3996823d71316e
hozlucas28 <hozlucas28@gmail.com>
2023-04-01 22:49:55 -0300

    Update main files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Header from './components/Header.jsx'
+import Main from './components/Main.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
 function App() {
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<Header />
+			<Main />
+			<Footer />
 		</>
 	)
 }
