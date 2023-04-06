92aa27c81f0b9b3eee47b700eee251955408ab9d
hozlucas28 <hozlucas28@gmail.com>
2023-04-06 16:52:33 -0300

    Create to-do lists examples

---
+++ src/constants/TO_DO_LISTS.js
@@ -0,0 +1,41 @@
+export const TO_DO_LISTS = {
+	1: {
+		title: 'To-do list 1 (example)',
+		checkboxs: [
+			{
+				id: 1,
+				text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+				completed: true,
+			},
+
+			{
+				id: 2,
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni!',
+				completed: false,
+			},
+		],
+	},
+
+	2: {
+		title: 'To-do list 2 (example)',
+		checkboxs: [
+			{
+				id: 1,
+				text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, eum.',
+				completed: false,
+			},
+
+			{
+				id: 2,
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit.',
+				completed: false,
+			},
+
+			{
+				id: 3,
+				text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, inventore.',
+				completed: true,
+			},
+		],
+	},
+}
