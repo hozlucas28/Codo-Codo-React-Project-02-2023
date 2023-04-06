ce3a65b7d7d9ce6a3aee5fb7aa7e051bcaff6a17
hozlucas28 <hozlucas28@gmail.com>
2023-04-06 16:38:38 -0300

    Add new animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				zoomIn: 'zoomIn 0.5s ease-in-out',
+			},
+
+			keyframes: {
+				zoomIn: {
+					'0%': { opacity: '0', transform: 'scale3d(0.3, 0.3, 0.3)' },
+					'50%': { opacity: '1' },
+				},
+			},
+		},
 	},
 	plugins: [],
 }
